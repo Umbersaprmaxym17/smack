bc8d81f99a21689c0cdcbb9a0f5f47584b0f5ec3
Umbersaprmaxym17 <maximsaprykin@yahoo.com>
2019-01-09 14:40:59 -0800

    create new user services added

--- app/src/main/java/com/example/max/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/max/smack/Controller/CreateUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.max.smack.R
 import com.example.max.smack.Services.AuthService
+import com.example.max.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -52,6 +53,7 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
@@ -59,9 +61,15 @@ class CreateUserActivity : AppCompatActivity() {
             if (registerSuccess){
                 AuthService.loginUser(this,email,password) {loginSuccess->
                     if(loginSuccess){
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
-                    }
+                        AuthService.createUser(this, userName, email, userAvatar, avatarColor){ createSuccess ->
+                            if (createSuccess){
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+                            }
+                        }
+                }
 
                 }
 
